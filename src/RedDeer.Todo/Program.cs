3b8c86cc2d4f028fd222ac050c653d5fadfc9763
Joe Mitchard <joe.mitchard@reddeer.com>
2022-07-12 10:28:48 +0100

    bootstrap backend project

---
+++ src/RedDeer.Todo/Program.cs
@@ -0,0 +1,34 @@
+const string localHostOrigins = "_localHostOrigins";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(opts => opts.AddPolicy(name: localHostOrigins, policy =>
+{
+    policy.WithOrigins("localhost:3000", "http://localhost:3000");
+}));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(localHostOrigins);
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
